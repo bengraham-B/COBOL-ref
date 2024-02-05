7f586996e36705d8f6aa0a43118c52b11ba6822e
beng <beng@reutech.co.za>
2024-02-05 14:32:55 +0200

    Working on a video tutorial

--- Variables_Datatypes.cob
+++ Variables_Datatypes.cob
@@ -9,5 +9,12 @@
        01 JustNums PIC 9(4) VALUE 1234.
        01 SignedInt PIC s9(4) VALUE -1234.
        01 PayCheck PIC 9(4)V99 VALUE ZEROS.
-       
+
+       01 Customer.
+           02 Ident PIC 9(3).
+           02 CustName PIC x(20).
+           02 DateOfBirth.
+               03 MOB PIC 99.
+               03 DOB PIC 99.
+               03 Y
        PROCEDURE DIVISION
