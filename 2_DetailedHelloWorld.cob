340bb796e2a9c174f6ca6ccbb3d2030eea5cf460
beng <beng@reutech.co.za>
2024-02-05 09:59:56 +0200

    init repo

---
+++ 2_DetailedHelloWorld.cob
@@ -0,0 +1,11 @@
+            IDENTIFICATION DIVISION.
+             PROGRAM-ID. HELLO.
+             AUTHOR. GOOSE_CPT
+             PROCEDURE DIVISION.
+                 A00-FIRST-PARA.
+                         DISPLAY "STRUCTURE OF A COBOL PROGRAM".
+                         DISPLAY 'Hello, world'.
+            
+                 A00-SECOND-PARA.
+                         DISPLAY "GOOSE".
+             STOP RUN.
